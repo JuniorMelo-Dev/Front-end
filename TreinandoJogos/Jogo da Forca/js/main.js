5925e7f12a19de180e0430d47c52c79aabafb672
JuniorMelo <juniormelo.dev@gmail.com>
2022-09-19 13:38:31 -0300

    :package:Finalização e teste de responsividade

--- TreinandoJogos/Jogo da Forca/js/main.js
+++ TreinandoJogos/Jogo da Forca/js/main.js
@@ -2,8 +2,6 @@ let tentativas = 6;
 let listaDinamica = [];
 let palavraSecretaCategoria;
 let palavraSecretaSorteada;
-
-
 const palavras = [
       palavra001 = {
           nome: "IRLANDA",
@@ -257,51 +255,135 @@ function criarPalavraSecreta() {
 
       palavraSecretaSorteada = palavras[indexPalavra].nome;
       palavraSecretaCategoria = palavras[indexPalavra].categoria;
-
-      console.log(palavraSecretaSorteada)
-      console.log(palavraSecretaCategoria)
 }
 
-montarPalavraTela();
-function montarPalavraTela() {
-      const categoria = document.getElementById("categoria");
-      categoria.innerHTML = palavraSecretaCategoria;
+montarPalavraNaTela();
+function montarPalavraNaTela() {
+    const categoria = document.getElementById("categoria");
+    categoria.innerHTML = palavraSecretaCategoria;
 
-      const palavraTela = document.getElementById("palavra-secreta");
-      palavraTela.innerHTML = "";
-
-      for(i = 0; i < palavraSecretaSorteada.length; i++) {
-            if(listaDinamica[i] == undefined) {
-                  listaDinamica[i] = "&nbsp;"
-                  palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
-            }else {
-                  palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
+    const palavraTela = document.getElementById("palavra-secreta");
+    palavraTela.innerHTML = "";
+    
+    for(i = 0; i < palavraSecretaSorteada.length; i++){  
+        if(listaDinamica[i] == undefined){
+            if (palavraSecretaSorteada[i] == " ") {
+                listaDinamica[i] = " ";
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letrasEspaco'>" + listaDinamica[i] + "</div>"
             }
-      }
+            else{
+                listaDinamica[i] = "&nbsp;"
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
+            }     
+        }
+        else{
+            if (palavraSecretaSorteada[i] == " ") {
+                listaDinamica[i] = " ";
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letrasEspaco'>" + listaDinamica[i] + "</div>"
+            }
+            else{
+                palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
+            }    
+        }
+    }   
 }
 
-function verificarLetra(letra) {
-    if(tentativas > 0) {
-        mudarStyleLetra("tecla-" + letra);
-        compararListas(letra);
+function verificarLetra(letra){
+    document.getElementById("tecla-" + letra).disabled = true;
+    if(tentativas > 0)
+    {
+        mudarStyleLetra("tecla-" + letra, false);
+        comparalistas(letra);
+        montarPalavraNaTela();
+    }    
+}
+
+function mudarStyleLetra(tecla, condicao){
+    if(condicao == false)
+    {
+        document.getElementById(tecla).style.background = "#C71585";
+        document.getElementById(tecla).style.color = "#ffffff";
     }
+    else{
+        document.getElementById(tecla).style.background = "#008000";
+        document.getElementById(tecla).style.color = "#ffffff";
+    }
+
+    
 }
 
-function mudarStyleLetra(tecla) {
-    document.getElementById(tecla).style.background = "#C71585";
-    document.getElementById(tecla).style.color = "#ffffff";
-      
-}
+function comparalistas(letra){
+    const pos = palavraSecretaSorteada.indexOf(letra)
+    if(pos < 0){
+        tentativas--
+        carregaImagemForca();
 
-function compararListas(letra) {
-    const posicao = palavraSecretaSorteada.indexOf(letra)
-    if (posicao < 0) {
-        tentativas --
-    }else {
-        for (i = 0; i < palavraSecretaSorteada.lenght; i ++) {
-            if (palavraSecretaSorteada[i] == letra) {
+        if(tentativas == 0){
+            abreModal("OPS!", "Não foi dessa vez ... A palavra secreta era <br>" + palavraSecretaSorteada);
+        }
+    }
+    else{
+        mudarStyleLetra("tecla-" + letra, true);
+        for(i = 0; i < palavraSecretaSorteada.length; i++){
+            if(palavraSecretaSorteada[i] == letra){
                 listaDinamica[i] = letra;
             }
         }
     }
+    
+    let vitoria = true;
+    for(i = 0; i < palavraSecretaSorteada.length; i++){
+        if(palavraSecretaSorteada[i] != listaDinamica[i]){
+            vitoria = false;
+        }
+    }
+
+    if(vitoria == true)
+    {
+        abreModal("PARABÉNS!", "Você venceu...");
+        tentativas = 0;
+    }
 }
+
+function carregaImagemForca(){
+    switch(tentativas){
+        case 5:
+            document.getElementById("imagem").style.background  = "url('./img/forca01.png')";
+            break;
+        case 4:
+            document.getElementById("imagem").style.background  = "url('./img/forca02.png')";
+            break;
+        case 3:
+            document.getElementById("imagem").style.background  = "url('./img/forca03.png')";
+            break;
+        case 2:
+            document.getElementById("imagem").style.background  = "url('./img/forca04.png')";
+            break;
+        case 1:
+            document.getElementById("imagem").style.background  = "url('./img/forca05.png')";
+            break;
+        case 0:
+            document.getElementById("imagem").style.background  = "url('./img/forca06.png')";
+            break;
+        default:
+            document.getElementById("imagem").style.background  = "url('./img/forca.png')";
+            break;
+    }
+}
+
+function abreModal(titulo, mensagem){
+    let modalTitulo = document.getElementById("exampleModalLabel");
+    modalTitulo.innerText = titulo;
+
+    let modalBody = document.getElementById("modalBody");
+    modalBody.innerHTML = mensagem;
+
+    $("#myModal").modal({
+        show: true
+    });
+}
+
+let btnReiniciar = document.querySelector("#btnReiniciar")
+btnReiniciar.addEventListener("click", function(){
+    location.reload();
+});
