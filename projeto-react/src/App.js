61f8eeac62f3051689d5999e1cb4b2e6349c9b68
JuniorMelo <juniormelo.dev@gmail.com>
2022-10-30 16:54:06 -0300

    ReactJS initial settings

---
+++ projeto-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React!!</h1>
+      <p>Meu primeiro app em ReactJS</p>
+    </div>
+  );
+}
+
+export default App;
